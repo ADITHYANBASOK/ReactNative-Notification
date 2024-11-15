c0d6a4d70090e8759664b30a08203b8797714a3a
Adithyan B Asok <adithyanbasok@gmail.com>
2024-11-15 10:25:58 +0530

    App.js Modified

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async()=>{
+    return{
+      shouldPlaySound:false,
+      shouldSetBadge:false,
+      shouldShowAlert:true,
+    }
+  }
+})
 
 export default function App() {
+  useEffect(()=>{
+    const getPermissions = async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      if (status !== 'granted') {
+        await Notifications.requestPermissionsAsync();
+      }
+    };
+    getPermissions();
+    const subscribe =Notifications.addNotificationReceivedListener((notification)=>{
+      console.log("Notification received");
+      console.log(notification);
+      const name= notification.request.content.data.user;
+      console.log(name);
+      
+    });
+
+    const subscribe2 = Notifications.addNotificationResponseReceivedListener((response)=>{
+      console.log("response recieved");
+      console.log(response)
+    });
+    return()=>{
+      subscribe.remove();
+      subscribe2.remove();
+    }
+  })
+
+  function getNotification(){
+    Notifications.scheduleNotificationAsync({
+      content:{
+        title:"Notification at your disposal",
+        body:"This is my First Notification",
+        data:{user:"John"},
+      },
+      trigger:{
+        seconds:3,
+      }
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Notify" onPress={getNotification}/>
     </View>
   );
 }
